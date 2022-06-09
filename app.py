f97a330cba922390bb79ca4854e4cea6e9194bab
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2022-06-09 23:29:34 +0600

    Added debug toolbar

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask
 
 from blueprints.page import page
 
+from extensions import debug_toolbar
 
-def create_app():
+
+def create_app(settings_override=None):
     """
     Create a Flask application using the app factory pattern.
 
+    :param settings_override: Override settings
     :return: Flask app
     """
     app = Flask(__name__, instance_relative_config=True)
@@ -14,9 +17,25 @@ def create_app():
     app.config.from_object('config.settings')
     app.config.from_pyfile('settings.py', silent=True)
 
+    if settings_override:
+        app.config.update(settings_override)
+
     app.register_blueprint(page)
+    extensions(app)
 
     return app
+
+
+def extensions(app):
+    """
+    Register 0 or more extensions (mutates the app passed in).
+
+    :param app: Flask application instance
+    :return: None
+    """
+    debug_toolbar.init_app(app)
+
+    return None
 
 
 if __name__ == '__main__':
